4fdd939e23cd1b4cc4d33143600b08a44420f9fb
Arisa <pisareva.alisa@gmail.com>
2024-09-18 22:07:11 -0400

    first commit

---
+++ src/test/java/com/example/glab31012/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.glab31012;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
